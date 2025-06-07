68dda057d7f778baf88daa9fb86fb749c59a7381
Keven Leone <keven.santos.sz@gmail.com>
2025-06-07 11:38:32 -0300

    AutoGen

---
+++ src/client/index.ts
@@ -0,0 +1,7 @@
+import { deleteProduct } from "./headless-commerce-admin-catalog-v1.0";
+
+deleteProduct({ path: { id: "123" } });
+
+// Product.getProduct({query:})
+
+Product.getProductsPage({ query: {} });
